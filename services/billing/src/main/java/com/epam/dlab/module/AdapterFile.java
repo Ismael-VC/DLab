86ac27f487f380a78adaaf4edd1e22dd044e888d
Bogdan_Rudyi <bogdan_rudyi@epam.com>
2017-10-20 19:20:42 +0300

    Revert as inner streams/readers don't require explicit closing, Sonar-L???(unregistered)

--- services/billing/src/main/java/com/epam/dlab/module/AdapterFile.java
+++ services/billing/src/main/java/com/epam/dlab/module/AdapterFile.java
@@ -1,20 +1,20 @@
 /***************************************************************************
 
-Copyright (c) 2016, EPAM SYSTEMS INC
+ Copyright (c) 2016, EPAM SYSTEMS INC
 
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
 
-    http://www.apache.org/licenses/LICENSE-2.0
+ http://www.apache.org/licenses/LICENSE-2.0
 
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
 
-****************************************************************************/
+ ****************************************************************************/
 
 package com.epam.dlab.module;
 
@@ -41,134 +41,127 @@ import com.google.common.base.MoreObjects.ToStringHelper;
  */
 @JsonTypeName(ModuleName.ADAPTER_FILE)
 @JsonClassDescription(
-	"File adapter.\n" +
-	"Read source or write converted data to the file.\n" +
-	"  - type: " + ModuleName.ADAPTER_FILE + "\n" +
-	"    [writeHeader: <true | false>]  - write header of data to the adapterOut.\n" +
-	"    file: <filename>               - the name of file."
-	)
+        "File adapter.\n" +
+                "Read source or write converted data to the file.\n" +
+                "  - type: " + ModuleName.ADAPTER_FILE + "\n" +
+                "    [writeHeader: <true | false>]  - write header of data to the adapterOut.\n" +
+                "    file: <filename>               - the name of file."
+)
 public class AdapterFile extends AdapterBase {
 
-	/** The name of file. */
-	@NotNull
-	@JsonProperty
-	private String file;
-	
-	
-	/** Default constructor for deserialization. */
-	public AdapterFile() { }
-	
-	/** Instantiate adapter for reading or writing.
-	 * @param mode the mode of adapter.
-	 */
-	public AdapterFile(Mode mode) {
-		super(mode);
-	}
+    /** The name of file. */
+    @NotNull
+    @JsonProperty
+    private String file;
 
-    
-	/** Return the name of file. */
-	public String getFile() {
-		return file;
-	}
-	
-	/** Set the name of file. */
-	public void setFile(String file) {
-		this.file = file;
-	}
-	
-	
-	/** Reader for adapter. */
-	@JsonIgnore
-	private BufferedReader reader;
-	
-	/** Writer for adapter. */
-	@JsonIgnore
-	private BufferedWriter writer;
-	
 
-	@Override
-	public void open() throws AdapterException {
-		try {
-			if (getMode() == Mode.READ) {
-				try (BufferedReader reader0 = new BufferedReader(new FileReader(file))) {
-					reader = reader0;
-				}
-			} else if (getMode() == Mode.WRITE) {
-				try (BufferedWriter writer0 = new BufferedWriter(new FileWriter(file))) {
-					writer = writer0;
-				}
-			} else {
-				throw new AdapterException("Mode of adapter unknown or not defined. Set mode to " + Mode.READ + " or " + Mode.WRITE + ".");
-			}
-		} catch (Exception e) {
-			throw new AdapterException("Cannot open file " + file + ". " + e.getLocalizedMessage(), e);
-		}
-		finally {
-			close();
-		}
-	}
+    /** Default constructor for deserialization. */
+    public AdapterFile() { }
 
-	@Override
-	public void close() throws AdapterException {
-		if (reader != null) {
-			try {
-				reader.close();
-			} catch (IOException e) {
-				throw new AdapterException("Cannot close file " + file + ". " + e.getLocalizedMessage(), e);
-			} finally {
-				reader = null;
-			}
-		}
+    /** Instantiate adapter for reading or writing.
+     * @param mode the mode of adapter.
+     */
+    public AdapterFile(Mode mode) {
+        super(mode);
+    }
 
-		if (writer != null) {
-			try {
-				writer.close();
-			} catch (IOException e) {
-				throw new AdapterException("Cannot close file " + file + ". " + e.getLocalizedMessage(), e);
-			} finally {
-				writer = null;
-			}
-		}
-	}
 
-	@Override
-	public String getEntryName() {
-		return getFile();
-	}
+    /** Return the name of file. */
+    public String getFile() {
+        return file;
+    }
 
-	@Override
-	public String readLine() throws AdapterException {
-		try {
-			return reader.readLine();
-		} catch (IOException e) {
-			throw new AdapterException("Cannot read file " + file + ". " + e.getLocalizedMessage(), e);
-		}
-	}
+    /** Set the name of file. */
+    public void setFile(String file) {
+        this.file = file;
+    }
 
-	@Override
-	public void writeHeader(List<String> header) throws AdapterException {
-		try {
-			writer.write(CommonFormat.rowToString(header));
-			writer.write(System.lineSeparator());
-		} catch (IOException e) {
-			throw new AdapterException("Cannot write file " + file + ". " + e.getLocalizedMessage(), e);
-		}
-	}
-	
-	@Override
-	public void writeRow(ReportLine row) throws AdapterException {
-		try {
-			writer.write(CommonFormat.rowToString(row));
-			writer.write(System.lineSeparator());
-		} catch (IOException e) {
-			throw new AdapterException("Cannot write file " + file + ". " + e.getLocalizedMessage(), e);
-		}
-	}
-	
-	
-	@Override
-	public ToStringHelper toStringHelper(Object self) {
-		return super.toStringHelper(self)
-				.add("file", file);
-	}
+
+    /** Reader for adapter. */
+    @JsonIgnore
+    private BufferedReader reader;
+
+    /** Writer for adapter. */
+    @JsonIgnore
+    private BufferedWriter writer;
+
+
+    @Override
+    public void open() throws AdapterException {
+        try {
+            if (getMode() == Mode.READ) {
+                reader = new BufferedReader(new FileReader(file));
+            } else if (getMode() == Mode.WRITE) {
+                writer = new BufferedWriter(new FileWriter(file));
+            } else {
+                throw new AdapterException("Mode of adapter unknown or not defined. Set mode to " + Mode.READ + " or " + Mode.WRITE + ".");
+            }
+        } catch (Exception e) {
+            throw new AdapterException("Cannot open file " + file + ". " + e.getLocalizedMessage(), e);
+        }
+    }
+
+    @Override
+    public void close() throws AdapterException {
+        if (reader != null) {
+            try {
+                reader.close();
+            } catch (IOException e) {
+                throw new AdapterException("Cannot close file " + file + ". " + e.getLocalizedMessage(), e);
+            } finally {
+                reader = null;
+            }
+        }
+
+        if (writer != null) {
+            try {
+                writer.close();
+            } catch (IOException e) {
+                throw new AdapterException("Cannot close file " + file + ". " + e.getLocalizedMessage(), e);
+            } finally {
+                writer = null;
+            }
+        }
+    }
+
+    @Override
+    public String getEntryName() {
+        return getFile();
+    }
+
+    @Override
+    public String readLine() throws AdapterException {
+        try {
+            return reader.readLine();
+        } catch (IOException e) {
+            throw new AdapterException("Cannot read file " + file + ". " + e.getLocalizedMessage(), e);
+        }
+    }
+
+    @Override
+    public void writeHeader(List<String> header) throws AdapterException {
+        try {
+            writer.write(CommonFormat.rowToString(header));
+            writer.write(System.lineSeparator());
+        } catch (IOException e) {
+            throw new AdapterException("Cannot write file " + file + ". " + e.getLocalizedMessage(), e);
+        }
+    }
+
+    @Override
+    public void writeRow(ReportLine row) throws AdapterException {
+        try {
+            writer.write(CommonFormat.rowToString(row));
+            writer.write(System.lineSeparator());
+        } catch (IOException e) {
+            throw new AdapterException("Cannot write file " + file + ". " + e.getLocalizedMessage(), e);
+        }
+    }
+
+
+    @Override
+    public ToStringHelper toStringHelper(Object self) {
+        return super.toStringHelper(self)
+                .add("file", file);
+    }
 }
